f16dc8c18a53a6b115a06c25fb8fed59482f0934
ibrahemkhaled74 <ibrahemkhaled1907@gmail.com>
2024-01-02 17:08:50 +0200

    add Speciality , updated vet entity

--- pet-clinic-data/src/main/java/com/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/petclinic/model/Speciality.java
@@ -3,10 +3,8 @@ package com.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Set;
-
 @Setter
 @Getter
-public class Vet extends Person {
-    Set<Speciality>specialities;
+public class Speciality extends BaseEntity{
+    String description;
 }
